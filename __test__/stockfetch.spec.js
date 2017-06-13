0a0815928e54dec3ef54fa0d4cb3ae2ac2259b4c
okandas <okandasjr@gmail.com>
2017-06-13 22:56:30 +0200

    read function done with tests

--- __test__/stockfetch.spec.js
+++ __test__/stockfetch.spec.js
@@ -1,7 +1,69 @@
+var td = require('testdouble')
+var fs = require('fs')
+var Stockfetch = require('../src/Stockfetch')
+
 describe('stockfetch tests', function () {
-  it('should pass this canary test', function () {
+  var stockfetch
+
+  beforeEach(() => {
+    stockfetch = new Stockfetch()
+  })
+
+  afterEach(() => {
+    td.reset()
+  })
+
+  it('should pass this canary test', () => {
     test('we should be ready to go', () => {
       expect(true).toBe(true)
     })
+  })
+
+  it('read should invoke error handler for invalid file', done => {
+    var onError = err => {
+      expect(err).toEqual('error reading file: invalidfile')
+      done()
+    }
+
+    td.replace(fs, 'readFile', (filename, callback) => {
+      callback(new Error('failed'))
+    })
+
+    stockfetch.readTickers('invalidfile', onError)
+  })
+
+  it('read should invoke processTickers for valid file', done => {
+    var rawData = 'GOOG\nAAPL\nTSLA\nMSFT'
+    var parsedData = ['GOOG', 'AAPL', 'TSLA', 'MSFT']
+
+    td.replace(stockfetch, 'parseTickers')
+    td.when(stockfetch.parseTickers(rawData)).thenReturn(parsedData)
+
+    td.replace(stockfetch, 'processTickers', (data) => {
+      expect(data).toEqual(parsedData)
+      done()
+    })
+
+    td.replace(fs, 'readFile', (filename, callback) => {
+      callback(null, rawData)
+    })
+
+    stockfetch.readTickers('tickers.txt')
+  })
+
+  it('read should return error if given file is empty', done => {
+    var onError = err => {
+      expect(err).toEqual('file tickers.txt has invalid content')
+      done()
+    }
+
+    td.replace(stockfetch, 'parseTickers')
+    td.when(stockfetch.parseTickers('')).thenReturn([])
+
+    td.replace(fs, 'readFile', (filename, callback) => {
+      callback(null, '')
+    })
+
+    stockfetch.readTickers('tickers.txt', onError)
   })
 })
--- src/Stockfetch.js
+++ src/Stockfetch.js
@@ -1,5 +1,33 @@
-var Stockfetch = function () {
+var fs = require('fs')
 
+var Stockfetch = function () {
+  this.readTickers = (filename, onError) => {
+    var self = this
+
+    var processResponse = (err, data) => {
+      if (err) {
+        onError('error reading file: ' + filename)
+      } else {
+        var tickers = self.parseTickers(data.toString())
+
+        if (tickers.length === 0) {
+          onError('file ' + filename + ' has invalid content')
+        } else {
+          self.processTickers(tickers)
+        }
+      }
+    }
+
+    fs.readFile(filename, processResponse)
+  }
+
+  this.parseTickers = () => {
+
+  }
+
+  this.processTickers = () => {
+
+  }
 }
 
 module.exports = Stockfetch
